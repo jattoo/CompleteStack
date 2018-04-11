c327d2a9d11631ea956112e19bb52be4f6de6ea9
jattoo <m_krub@aol.com>
2018-04-11 19:01:15 +0300

    Uft-8 kirjaimet kunnostettu

--- luku1palautukset/tehtava11.js
+++ luku1palautukset/tehtava11.js
@@ -15,7 +15,7 @@ const Sisalto = (props) => {
 
 const Yhteensa = (props) => {
     return (
-        <p>yhteens‰ {props.kaikki} teht‰v‰‰</p>
+        <p>{props.sana1} {props.kaikki} {props.sana2}</p>
     )
 }
 
@@ -24,18 +24,20 @@ const App = () => {
     const kurssi = "Half Stack -sovelluskehitys"
     const osa1 = "Reactin perusteet "
     const tehtavia1 = 10
-    const osa2 = "Tiedonv‰litys propseilla "
+    const osa2 = "Tiedonv\u00e4litys propseilla "
     const tehtavia2 = 7
     const osa3 = "Komponenttien tila "
     const tehtavia3 = 14
     const yheensa = tehtavia1 + tehtavia2 + tehtavia3
+    const sana1 = 'yhteens\u00e4'
+    const sana2 = 'teht\u00e4v\u00e4\u00e4'
     return (
         <div>
             <Otsikko kurssi={kurssi} />
             <Sisalto osa={osa1} tehtavia={tehtavia1} />
             <Sisalto osa={osa2} tehtavia={tehtavia2} />
             <Sisalto osa={osa3} tehtavia={tehtavia3} />
-            < Yhteensa kaikki={yheensa} />
+            < Yhteensa sana1={sana1} kaikki={yheensa} sana2={sana2} />
         </div>
     )
 }
