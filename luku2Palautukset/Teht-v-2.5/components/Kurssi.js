2adc66f38a901aaa8c4f82a4b10ed99800365839
jattoo <m_krub@aol.com>
2018-04-30 19:11:57 +0300

    finaali version palautus

--- luku2Palautukset/Teht-v-2.5/components/Kurssi.js
+++ luku2Palautukset/Teht-v-2.5/components/Kurssi.js
@@ -3,32 +3,13 @@ import React from 'react'
 //Tehtävä 2.5 pysyy samana kuin tehtävä 2.4
 //Komponentti kurssi exporttaa itsensä nimellä Kurssi alhäällä
 
+
 const Kurssi = ({ kurssi }) => {
-    const results = () => kurssi.map(kursit => 
-        <div>
-            <h3 key ={kursit.id}>
-                {kursit.nimi}
-                {kursit.osat.map(k => 
-                <p key ={k.id}>
-                    {k.nimi} {k.tehtavia}                
-                </p>
-            )}
-           </h3>
-           <h4>
-            {'Yhteensä '} 
-            {kursit.osat.reduce((nyt, yht) =>
-                 nyt +yht.tehtavia , 0
-                )
-            }
-            {' Tehtäviä'}
-            </h4>
-        </div>
-    )
-  
     return (
-        <div > 
-            <h1>{'Opetusohjelma'}</h1>
-            {results()}
+        <div >
+            <h2>{kurssi.nimi}</h2>
+            <h3>{kurssi.osat.map(k2 => <p key = {k2.id}> {k2.nimi} {k2.tehtavia}</p>)}</h3>
+            <h3>{'Yhteensä'} {kurssi.osat.reduce((nyt, yht) => nyt + yht.tehtavia, 0)} {'tehtäviä'}</h3>  
         </div>
     )
 }
