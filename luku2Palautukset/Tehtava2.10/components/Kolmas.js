d7233d17d2b8fabd0feef0e3ac1153dae6f099e8
jattoo <m_krub@aol.com>
2018-05-04 11:02:20 +0300

    Tehtävän palautus

--- luku2Palautukset/Tehtava2.10/components/Kolmas.js
+++ luku2Palautukset/Tehtava2.10/components/Kolmas.js
@@ -1,1 +1,12 @@
+import React from 'react'
 
+const Kolmas = ({props}) => {
+    return (
+      <h3>
+            {props.name} {props.numero}
+      </h3>
+    )
+}
+
+
+export default Kolmas
--- luku2Palautukset/Tehtava2.10/components/Note.js
+++ luku2Palautukset/Tehtava2.10/components/Note.js
@@ -1,1 +1,13 @@
+import React from 'react'
 
+const Note = ({note}) => {
+    return (
+        <h3>
+            {note.name} {note.numero}
+        </h3>
+    )
+
+}
+
+
+export default Note
