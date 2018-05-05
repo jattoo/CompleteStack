3710023164e9ec68d363e216fcca690ca2c16b2e
jattoo <m_krub@aol.com>
2018-05-05 21:02:21 +0300

    Tehtava 2.11 palautus

--- luku2Palautukset/Tehtava2.11/src/App.js
+++ luku2Palautukset/Tehtava2.11/src/App.js
@@ -1,1 +1,45 @@
+import React from 'react'
+import Note from './components/Note'
+import axios from 'axios'
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {    
+      notes: [],
+      newNote: '',
+      showAll : true
+    }
+    console.log("Test 1: Contructor")
+  }
+
+  componentWillMount(){
+    console.log("Test 2: ComponentWillmount")
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        this.setState({ notes: response.data})
+      })
+  }
+
+  
+
+  render(){
+    return (
+      <div>
+        <h3>PUHELINLUETTELO</h3>
+        <ul>
+          {this.state.notes.map(n => 
+          <Note key={n.id} note={n} />)}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
+
+
 
