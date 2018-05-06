c94e149ce424b1a46e8f18177a32d63cdd598a4a
jattoo <m_krub@aol.com>
2018-05-06 14:19:49 +0300

    Tehtavan palautus

---
+++ luku2Palautukset/Tehtava2.12/components/Note.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Note = ({note}) => {
+    return (
+        <div>
+        <li>
+            {note.name}
+        </li>
+        
+        </div>
+    )
+
+}
+
+
+
+export default Note
