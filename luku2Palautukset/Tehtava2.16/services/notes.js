b8b929f3c29b8ef668679a66a7b3841279015723
jattoo <m_krub@aol.com>
2018-05-12 00:13:10 +0300

    Tehtava 2.16 palautus

--- luku2Palautukset/Tehtava2.16/services/notes.js
+++ luku2Palautukset/Tehtava2.16/services/notes.js
@@ -1,1 +1,21 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
 
+const getAll = () =>{
+    const req = axios.get(baseUrl)
+    return req.then(res => {return res.data})
+}
+
+const newPerson = (obj) =>{
+    const req = axios.post(baseUrl, obj)
+    return req.then(res => res.data)
+}
+
+const upDate = (id, obj) => {
+    const req = axios.delete(`${baseUrl}/${id}`, obj)
+    return req.then(res => res.data)
+}
+
+
+
+export default {getAll, newPerson, upDate}
