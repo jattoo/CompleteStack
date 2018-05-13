fa47154e5f2e2d4ce34166ee4c18ec95fd059191
Momodou Krubally <jattoo@users.noreply.github.com>
2018-05-13 12:04:10 +0300

    Update Notification.js

--- luku2Palautukset/Tehtava2.18/src/components/Notification.js
+++ luku2Palautukset/Tehtava2.18/src/components/Notification.js
@@ -1,1 +1,15 @@
 
+import React from 'react'
+
+const Notification = ({ viestit }) => {
+    if (viestit === null ){
+        return null
+    }
+    return (
+        <div className= "error">
+            {viestit}
+        </div>
+    )
+}
+
+export default Notification
