b9668698f70e5b4dbbbe3a4c5a033afa0047e589
jattoo <m_krub@aol.com>
2018-06-07 21:08:44 +0300

    apunfunktioita ja yksikkötestejä, osa 3

--- Blogilista/utils/list_helpers.js
+++ Blogilista/utils/list_helpers.js
@@ -8,5 +8,21 @@ const totalLikes = (blogs) => {
     return blogs[0].likes
 }
 
+const format = (obj) => {
+    return {
+        title : obj[0].title,
+        author: obj[0].author,
+        likes: obj[0].likes
+    }
+}
+//suosikkeja
+const favoriteBlog = (blogs) => {
+    let highest = 0
+    blogs.map(m => m.likes > highest ? highest = m.likes : m.likes)
 
-module.exports = {dummy, totalLikes}
+    const test = blogs.filter(f => f.likes === highest)
+    const copyofTest = format(test)
+    console.log('test ', copyofTest)
+    return copyofTest
+}
+module.exports = {dummy, totalLikes, favoriteBlog}
