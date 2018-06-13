e4da4bb080a047fefcaf9a14a5c00993e43b326f
jattoo <m_krub@aol.com>
2018-06-13 13:41:20 +0300

    blogilistan testit, osa 3

--- Blogilista/test/blog_api_test.js
+++ Blogilista/test/blog_api_test.js
@@ -17,7 +17,7 @@ describe('Api level tests', () => {
     })
 
     //blogilistan testit, osa 2
-    test('able to add new blogs', async () => {
+    test.skip('able to add new blogs', async () => {
         const origins = await api
             .get('/api/blogs')
         const demo = {
@@ -30,7 +30,7 @@ describe('Api level tests', () => {
         await api
             .post('/api/blogs')
             .send(demo)
-            .expect(201)
+            .expect(200)
             .expect('Content-Type', /application\/json/)
 
         const res = await api
@@ -38,6 +38,34 @@ describe('Api level tests', () => {
 
         expect(res.body.length).toBe(origins.body.length + 1)
     })
+
+    //blogilistan testit, osa 3
+    test('blogs without likes = 0', async () => {
+        //ennen blogin lisääminen
+        const origins = await api
+            .get('/api/blogs')
+
+        const newBlog = {
+            title: 'blogilistan testit, osa 3',
+            author: 'Me Minä',
+            url: 'https://example.com'
+        }
+        
+        if (newBlog.likes === undefined){
+            newBlog.likes = 0
+
+            await api
+                .post('/api/blogs')
+                .send(newBlog)
+                .expect(200)
+        }
+        //blogin lisäämisen jälkeen
+        const res = await api
+            .get('/api/blogs')
+
+        expect(res.body.length).toBe(origins.body.length + 1)
+        
+    })  
 
     
 })
