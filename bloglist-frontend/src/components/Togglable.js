01e70ddc1a17980d7b751c8c05067a3a256a09cf
jattoo <m_krub@aol.com>
2018-06-29 16:13:45 +0300

    blogilistan frontend, osa 5

---
+++ bloglist-frontend/src/components/Togglable.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+class Togglable extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            visible : false
+        }
+    }
+
+    //funktio huolehtii staten boolean arvon vaihtoon
+    toggleVisibility = () => {
+        this.setState({
+            visible : !this.state.visible
+        })
+    }
+
+    render(){
+        //muutujien avulla asetettaan css display methodin arvo ja muuttujat on valmis
+        //meille käyttöön
+        const hideWhenVisible = { display: this.state.visible ? 'none' : ''}
+        const showWhenVisible = { display: this.state.visible ? '' : 'none'}
+
+        return(
+            <div>
+                <div style={hideWhenVisible}>
+                    <button onClick={this.toggleVisibility}>{this.props.buttonLabel}</button>
+                </div>
+                <div style={showWhenVisible}>
+                    {this.props.children}
+                    <button onClick={this.toggleVisibility}>cancel</button>
+                </div>
+            </div>
+        )
+
+    }
+}
+
+export default Togglable
