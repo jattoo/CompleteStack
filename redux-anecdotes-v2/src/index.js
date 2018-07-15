68d49c5559bb5da20e11277e291efea4bcae6b2b
jattoo <m_krub@aol.com>
2018-07-15 21:55:27 +0300

    6.1 ESlint

---
+++ redux-anecdotes-v2/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import store from './store'
+import './index.css'
+const render = () => {
+    ReactDOM.render(
+        <App store={store} />,
+        document.getElementById('root')
+    )
+}
+render()
+store.subscribe(render)
