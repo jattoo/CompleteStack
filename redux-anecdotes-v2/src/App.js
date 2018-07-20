b8b9e6868c65d4f3a7bcb5c3939359fd30192fc4
jattoo <m_krub@aol.com>
2018-07-20 21:07:34 +0300

    6.10 anekdootit ja backend, osa 1

--- redux-anecdotes-v2/src/App.js
+++ redux-anecdotes-v2/src/App.js
@@ -2,7 +2,15 @@ import React from 'react'
 import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/Anecdote_list'
+import { connect } from 'react-redux'
+import { anectInit } from './reducers/anecdoteReducer'
+import anecdoteService from './services/notes'
+
 class App extends React.Component {
+    componentDidMount = async () => {
+        const anecdotes = await anecdoteService.getAll()
+        this.props.anectInit(anecdotes)
+    }
     render() {
         return (
             <div>
@@ -15,4 +23,9 @@ class App extends React.Component {
     }
 }
 
-export default App
+
+
+export default connect(
+    null,
+    { anectInit }
+)(App)
--- redux-anecdotes-v2/src/services/notes.js
+++ redux-anecdotes-v2/src/services/notes.js
@@ -1,1 +1,8 @@
+import axios from 'axios'
+const url = 'http://localhost:3001/anecdotes'
 
+const getAll = async () => {
+    const res = await axios.get(url)
+    return res.data
+}
+export default { getAll }
