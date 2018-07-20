3c3d3135c435e842fb89c8e2a169832356cc3ff0
jattoo <m_krub@aol.com>
2018-07-20 17:47:22 +0300

    6.9 paremmat anekdootit, osa 8

--- redux-anecdotes-v2/src/components/Anecdote_list.js
+++ redux-anecdotes-v2/src/components/Anecdote_list.js
@@ -8,78 +8,44 @@ import store from './../store'
 
 
 class AnecdoteList extends React.Component {
-   
-
     render() {
-        const { filter, anecdotes } = store.getState()
-        const showMe = anecdotes.find(note => note.content.toLowerCase().substr(0, 10) === filter)
-        if (showMe){
-            return (
+        return (
+            <div>
+                <h2>Anecdotes</h2>
                 <div>
-                    <h2>Anecdotes</h2>
-                    <div>
-                        <Filter />
-                    </div>
-                    {
-                        <div key={showMe.id}>
-                            <div>
-                                {showMe.content}
-                            </div>
-                            <div>
-                            has <a className="voteNumbers"><b>{showMe.votes}</b></a>{' '}
-                                <button onClick={() => 
-                                {this.props.votersCard(showMe.id),
-                                this.props.sendNotifications(showMe.content),
-                                setTimeout(() => {
-                                    this.props.clearAll()
-                                }, 5000)}
-                                }
-                                >vote</button>
-                            </div>
-                        </div>
-                    }
+                    <Filter />
                 </div>
-
-            ) 
-        } else {
-        
-            return (
-                <div>
-                    <h2>Anecdotes</h2>
-                    <div>
-                        <Filter />
-                    </div>
-                    {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
-                        <div key={anecdote.id}>
-                            <div>
-                                {anecdote.content}
-                            </div>
-                            <div>
-                            has <a className="voteNumbers"><b>{anecdote.votes}</b></a>{' '}
-                                <button onClick={() => 
-                                {store.dispatch(voting(anecdote.id)),
-                                store.dispatch(notifNews(anecdote.content)),
-                                setTimeout(() => {
-                                    store.dispatch(notifReset())
-                                }, 5000)}
-                                }
-                                >vote</button>
-                            </div>
+                {this.props.visibileNotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+                    <div key={anecdote.id}>
+                        <div>
+                            {anecdote.content}
                         </div>
-                    )}
-                </div>
-            )
-        }
+                        <div>
+                        has <a className="voteNumbers"><b>{anecdote.votes}</b></a>{' '}
+                            <button onClick={() => 
+                            {store.dispatch(voting(anecdote.id)),
+                            store.dispatch(notifNews(anecdote.content)),
+                            setTimeout(() => {
+                                store.dispatch(notifReset())
+                            }, 5000)}
+                            }
+                            >vote</button>
+                        </div>
+                    </div>
+                )}
+            </div>
+        )
     }
-    
 }
 
+const anecdotesToShow = (anecdote, notif, filter) => {
+    const showMe = anecdote.find(note => note.content.toLowerCase().substr(0, 10) === filter)
+    return showMe ? [showMe] : anecdote
+}
 
 const mapStateToProps = (store) => {
     return {
-        anecdote: store.anecdote,
-        notif: store.notif,
-        filter: store.filter
+        visibileNotes: anecdotesToShow(store.anecdotes,store.notif,store.filter)
     }
 }
 
