fc555c34d3ea318af6cb13bc8b37c4153f6c4e06
Momodou Krubally <jattoo@users.noreply.github.com>
2018-08-15 20:37:34 +0300

    Create middleware.js

---
+++ Bloglist-Sovellus/bloglist-backend/utils/middleware.js
@@ -0,0 +1,27 @@
+const logger= (req, res, next) => {
+
+    //test ympäristön konsolin siistittaminen
+    if (process.env.NODE_ENV === 'test'){
+        return next()
+    }
+    console.log('Method: ', req.method)
+    console.log('Path: ', req.path)
+    console.log('Body: ', req.body)
+    console.log('-----')
+    next()
+}
+
+const error= (req, res) => {
+    res.status(404).send({ error : 'unknown endpoint'})
+}
+
+
+const tokenExtractor = (req, res, next) => {
+    const authorization = req.get('authorization')
+    if(authorization && authorization.toLowerCase().startsWith('bearer ')){
+        req.token = authorization.substring(7)
+    }
+    next()
+}
+
+module.exports = {logger, error, tokenExtractor}
