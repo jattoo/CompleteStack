9e64c6bb49c7b75f80a98f6e2dcc348b440f5045
Momodou Krubally <jattoo@users.noreply.github.com>
2018-08-15 20:26:48 +0300

    Create login.js

---
+++ Bloglist-Sovellus/bloglist-backend/contollers/login.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+
+loginRouter.post('/', async (req, res) => {
+    //pyynön tuleva dataa haettaan tähän
+    const body = req.body
+
+    //etsitaan kannasta pynnön mukana tullut datan username
+    const user = await User.findOne({ username: body.username})
+
+
+    //varmistettaan salasana bcrypt kirjaston avulla
+    const realPassWord = user === null ?
+        false :
+        await bcrypt.compare(body.password, user.password)
+    console.log('user pass: ', user.password)
+    //jos vaarilla tiedoilla yritettään kirjautua ==> virhe
+    if  (!(user && realPassWord)){
+        return res.status(401).json({ error : 'Wrong credentials!'})
+    }
+
+    //grneroidaan tokenit pynntö tiedosta ja talettaa ne tähän jos kaikki hyvin
+    const userToken = {
+        username : user.username,
+        id: user._id
+    } 
+
+    //allekirjoitettaan jwt kirjastolla
+    const token = jwt.sign(userToken, process.env.SECRET)
+
+    res.status(200).send({token, username : user.username, name: user.name })
+
+})
+
+module.exports = loginRouter
