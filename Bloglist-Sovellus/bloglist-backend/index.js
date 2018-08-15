f90875dc8590f29de4d31dd104ccaf4c9e0ed82f
Momodou Krubally <jattoo@users.noreply.github.com>
2018-08-15 20:38:37 +0300

    Create index.js

---
+++ Bloglist-Sovellus/bloglist-backend/index.js
@@ -0,0 +1,47 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const blogRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
+
+//yhteys kannan väliin muodestettään täällä
+mongoose
+    .connect(config.mongoUrl)
+    .then(() => {
+        console.log('Connection established to db', config.mongoUrl)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('dev'))
+app.use(middleware.logger)
+app.use(middleware.tokenExtractor)
+
+//controllers
+app.use('/api/blogs', blogRouter)
+app.use('/api/blogusers', usersRouter)
+app.use('/api/login', loginRouter)
+
+const server = http.createServer(app)
+
+
+server.listen(config.port, () => {
+    console.log(`Server running on port ${config.port}`)
+})
+
+server.on('close', () => {
+    mongoose.connection.close()
+})
+
+module.exports= { app, server } 
