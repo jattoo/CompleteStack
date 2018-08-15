381c9a8d98a963eb3a8f2519e716b9ba1b25a792
Momodou Krubally <jattoo@users.noreply.github.com>
2018-08-15 20:38:04 +0300

    Create .eslintrc.js

---
+++ Bloglist-Sovellus/bloglist-backend/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "jest": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType":"module",
+        "ecmaVersion": 2017
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "plugins": [
+            "react"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-unused-vars":"off"
+        
+    }
+};
