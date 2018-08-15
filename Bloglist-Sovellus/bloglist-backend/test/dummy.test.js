3465380d411cbf5613125f86da40d85193b5237a
Momodou Krubally <jattoo@users.noreply.github.com>
2018-08-15 20:34:21 +0300

    Create dummy.test.js

---
+++ Bloglist-Sovellus/bloglist-backend/test/dummy.test.js
@@ -0,0 +1,14 @@
+//require our test methods
+const listHelper = require('../utils/list_helper').dummy
+
+//Tuloksen muokaillu
+describe.skip('dummy', () => {
+    //ensimmäinen test
+    test('dummy is called', () => {
+        const blogs = []
+        const result = listHelper(blogs) 
+        
+        //odotettava tulos
+        expect(result).toBe(1)
+    })
+})
