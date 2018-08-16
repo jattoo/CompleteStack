6449aaac2d7bf9463a80e0f03af66ee4f42a8e05
Momodou Krubally <jattoo@users.noreply.github.com>
2018-08-16 11:49:09 +0300

    Create LoginForm.js

---
+++ Bloglist-Sovellus/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const LoginForm = ({ handleSubmit, handleChange, username, password }) => {
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+      <div>
+            <div>
+            Username: 
+              <input 
+                type="text"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              Password: 
+              <input 
+                type="password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </div>
+            <button>Login</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
+}
+
+export default LoginForm
