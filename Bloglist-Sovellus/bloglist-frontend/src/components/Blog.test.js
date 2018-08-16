4d8a1723bb053e6adf6cb62102ce24cf7227e8ba
Momodou Krubally <jattoo@users.noreply.github.com>
2018-08-16 11:48:33 +0300

    Create Blog.test.js

---
+++ Bloglist-Sovellus/bloglist-frontend/src/components/Blog.test.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Blog from './Blog'
+
+
+describe.skip('<Blog test />', () => {
+    let component, blog
+    beforeEach(() => {
+        blog = {
+            title: 'Having some blog test',
+            author: 'Kuningas Moe',
+            url: 'http://mitakuuluu.com',
+            likes: 100,
+            
+        }
+        const mockHandler = jest.fn()
+        component = shallow(
+        <Blog 
+            title={blog.title}
+            author={blog.author}
+            url={blog.url}
+            likes={blog.likes}
+            addLikes={() => mockHandler}
+            cancelLikes={() => mockHandler}
+            poisto={mockHandler}
+    
+        />)
+    })
+    
+    it('after clicking name the details are displayed', () =>{
+        const defaultContentDiv = component.find('.hideContent')
+        defaultContentDiv.simulate('click')
+
+        //alkutilan tarkistus
+        expect(defaultContentDiv.text()).toContain(blog.title)
+        expect(defaultContentDiv.text()).toContain(blog.author)
+        expect(defaultContentDiv.text()).not.toContain(blog.url)
+        expect(defaultContentDiv.text()).not.toContain(blog.likes)
+
+        const contentDiv = component.find('.showContent')
+        
+        //tapahtuman käsittelijän klikkaamisen jälkeen
+        expect(contentDiv.text()).toContain(blog.title)
+        expect(contentDiv.text()).toContain(blog.author)
+        expect(contentDiv.text()).not.toContain(blog.url)
+        expect(contentDiv.text()).not.toContain(blog.likes)
+    })
+})
