8b50017b491a408ce5047d864848947e894b3a26
Momodou Krubally <jattoo@users.noreply.github.com>
2018-09-17 23:25:50 +0300

    Create loginReducer.js

---
+++ Bloglist-Sovellus/webpackinFrontti/src/reducers/loginReducer.js
@@ -0,0 +1,22 @@
+const loginReducer = (state = [], action) => {
+    switch (action.type) {
+    case 'LOGIN_INIT':
+        //console.log('login data: ',action.data)
+        const doSomething = JSON.parse(action.data)
+        return doSomething
+    default:
+        return state
+    }
+}
+
+export const loginInit = () => {
+    return async (dispatch) => {
+        const userOnline = await window.localStorage.getItem('currentUser')
+        dispatch({
+            type: 'LOGIN_INIT',  
+            data: userOnline
+        })
+    }
+}
+
+export default loginReducer
